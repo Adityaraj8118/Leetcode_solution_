309ea6311622d96de80e1023e36f29c60095e2de
Aditya Raj Gupta <161880450+Adityaraj8118@users.noreply.github.com>
2025-03-06 23:53:10 +0530

    Find the difference.cpp

---
+++ Find the difference.cpp
@@ -0,0 +1,10 @@
+// Question no 389
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char c = 0;
+        for(char cs : s) c ^= cs;
+        for(char ct : t) c ^= ct;
+        return c;
+    }
+};
